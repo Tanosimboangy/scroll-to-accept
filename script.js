d2a2e072c7dacbaff3011c998e0beda84e21b44c
Tanosimboangy <jacquit.aja@onja.org>
2020-07-16 14:59:22 +0300

    creating the script

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('it works');
+const terms = document.querySelector('.terms-and-conditions');
+const button = document.querySelector('.accept');
+
+const obCallback = payload => {
+    if(payload[0].intersectionRatio === 1) {
+        button.disabled = false;
+        ob.unobserve(terms.lastElementChild);
+    }
+}
+
+const ob = new IntersectionObserver(obCallback, {
+    root: terms,
+    threshod: 1,
+});
+
+ob.observe(terms.lastElementChild);
